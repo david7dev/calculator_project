68d3fd8fe1c8bc1340527d27f1185d3adbf50d8f
David de Sousa <102882940+david7dev@users.noreply.github.com>
2026-06-10 22:24:05 -0300

    Update calculator.main.py

--- calculator.main.py
+++ calculator.main.py
@@ -10,6 +10,8 @@ def multiply(n1, n2):
     return n1 * n2
 
 def divide(n1, n2):
+    if n2 == 0:
+        raise ZeroDivisionError
     return n1 / n2
 
 operations = {
@@ -29,7 +31,13 @@ def calculator():
             print(symbol)
         operation_symbol = input("Choose an operation: ")
         num2 = float(input("Type the second number: "))
-        answer = operations[operation_symbol](num1, num2)
+
+        try:
+            answer = operations[operation_symbol](num1, num2)
+        except ZeroDivisionError:
+            print("Error: it's not possible divide by 0. Try again.\n")
+            continue
+
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
         choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.")
